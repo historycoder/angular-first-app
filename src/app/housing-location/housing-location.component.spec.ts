32b1b272c9818d8467a51c092a29c8e7e2c9d180
Lauren Mitchell <sortsearch@gmail.com>
2023-08-16 13:44:57 -0400

    Lesson 7: Add an interpolation to a component

--- src/app/housing-location/housing-location.component.spec.ts
+++ src/app/housing-location/housing-location.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HousingLocationComponent } from './housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 describe('HousingLocationComponent', () => {
   let component: HousingLocationComponent;
@@ -12,6 +13,16 @@ describe('HousingLocationComponent', () => {
     });
     fixture = TestBed.createComponent(HousingLocationComponent);
     component = fixture.componentInstance;
+    component.housingLocation = {
+      name: 'test',
+      id: 0,
+      city: 'test',
+      state: 'test',
+      photo: 'test',
+      availableUnits: 0,
+      wifi: false,
+      laundry: false
+    };
     fixture.detectChanges();
   });
 
